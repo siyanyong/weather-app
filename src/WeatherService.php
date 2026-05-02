a23f3cff44abe38eff52c0d9bc99951e44e3f002
Renny Su <rennie.su@gmail.com>
2026-05-02 17:09:11 +0800

    Initial

---
+++ src/WeatherService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+use GuzzleHttp\Client;
+
+class WeatherService
+{
+    private Client $client;
+
+    public function __construct(
+        private readonly string $apiKey = '22b6c497f1d70bab11b5a851154465d6',
+        private readonly string $apiUrl = 'https://api.openweathermap.org/data/2.5/weather',
+    ) {
+        $this->client = new Client(); // tightly couples to Guzzle
+    }
+
+    public function getWeather(string $city): array
+    {
+        $response = $this->client->get($this->apiUrl, [
+            // Add query parameters to the URL... ?q=city&appid
+            'query' => [
+                'q' => $city,
+                'appid' => $this->apiKey,
+                'units' => 'metric',
+            ]
+        ]);
+
+        $weatherData = json_decode($response->getBody()->getContents(), true); // true to return as associative array
+
+        return [
+            'city' => $weatherData['name'],
+            'temperature' => $weatherData['main']['temp'],
+            'description' => $weatherData['weather'][0]['description'],
+            'humidity' => $weatherData['main']['humidity']
+        ];
+    }
+}
+
+// We want to run it as such:
+// php weather.php Vienna
+
+/* DI
+public function __construct(
+    private readonly Client $client,
+    private readonly string $apiKey,
+    private readonly string $apiUrl
+) {}
+
+new WeatherService(
+    new Client(),
+    'key',
+    'url'
+);
+*/
---
+++ weather.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\WeatherService;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$weatherService = new WeatherService();
+$city = 'Singapore';
+$weather = $weatherService->getWeather($city);
+var_dump($weather);
+
+/*
+🔥 MOST IMPORTANT STEP (people miss this)
+
+After ANY change to:
+
+file names
+namespaces
+composer.json
+
+You MUST run:
+
+composer dump-autoload
+
+Without this → Composer still uses old class map → class “not found”.
+*/
